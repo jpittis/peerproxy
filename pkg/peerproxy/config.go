e39d54052a98641dbbec11493b2f965b1afd4d34
Jake Pittis <jakepittis@gmail.com>
2023-06-19 20:19:58 -0400

    Support outbound latency in config

--- pkg/peerproxy/config.go
+++ pkg/peerproxy/config.go
@@ -1,7 +1,9 @@
 package peerproxy
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v3"
 )
@@ -13,14 +15,18 @@ type Config struct {
 }
 
 type Listener struct {
-	Name         string `yaml:"name"`
-	ListenerAddr string `yaml:"listener_addr"`
-	UpstreamAddr string `yaml:"upstream_addr"`
+	Name            string                   `yaml:"name"`
+	ListenerAddr    string                   `yaml:"listener_addr"`
+	UpstreamAddr    string                   `yaml:"upstream_addr"`
+	OutboundLatency map[string]time.Duration `yaml:"outbound_latency"`
 }
 
 func ParseConfigBytes(buf []byte) (*Config, error) {
 	config := &Config{}
 	if err := yaml.Unmarshal(buf, config); err != nil {
+		return nil, err
+	}
+	if err := ValidateConfig(config); err != nil {
 		return nil, err
 	}
 	return config, nil
@@ -32,4 +38,29 @@ func ParseConfigFile(filename string) (*Config, error) {
 		return nil, err
 	}
 	return ParseConfigBytes(buf)
+}
+
+func ValidateConfig(config *Config) error {
+	listenerNames := map[string]struct{}{}
+	for _, ln := range config.Listeners {
+		if _, alreadyPresent := listenerNames[ln.Name]; alreadyPresent {
+			return fmt.Errorf("duplicate listener name %s", ln.Name)
+		}
+		listenerNames[ln.Name] = struct{}{}
+	}
+
+	for _, ln := range config.Listeners {
+		for name := range ln.OutboundLatency {
+			if name == ln.Name {
+				return fmt.Errorf("listener %s self reference in outbound latency", ln.Name)
+			} else if _, listenerExists := listenerNames[name]; !listenerExists {
+				return fmt.Errorf(
+					"outbound latency for listener %s references %s which does not exist",
+					ln.Name, name,
+				)
+			}
+		}
+	}
+
+	return nil
 }
--- pkg/peerproxy/config_test.go
+++ pkg/peerproxy/config_test.go
@@ -2,32 +2,66 @@ package peerproxy
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 var exampleConfig = `
 cluster_name: "etcd-cluster-1"
-destination: stdout
+destination: stderr
 listeners:
   - name: infra1
     listener_addr: "localhost:12381"
     upstream_addr: "localhost:12380"
+    outbound_latency:
+      infra2: 30ms
+      infra3: 30ms
   - name: infra2
     listener_addr: "localhost:22381"
     upstream_addr: "localhost:22380"
+    outbound_latency:
+      infra1: 30ms
+      infra3: 10ms
   - name: infra3
     listener_addr: "localhost:32381"
     upstream_addr: "localhost:32380"
+    outbound_latency:
+      infra1: 30ms
+      infra2: 10ms
 `
 
 var expectedExampleConfig = &Config{
 	ClusterName: "etcd-cluster-1",
-	Destination: "stdout",
+	Destination: "stderr",
 	Listeners: []*Listener{
-		{Name: "infra1", ListenerAddr: "localhost:12381", UpstreamAddr: "localhost:12380"},
-		{Name: "infra2", ListenerAddr: "localhost:22381", UpstreamAddr: "localhost:22380"},
-		{Name: "infra3", ListenerAddr: "localhost:32381", UpstreamAddr: "localhost:32380"},
+		{
+			Name:         "infra1",
+			ListenerAddr: "localhost:12381",
+			UpstreamAddr: "localhost:12380",
+			OutboundLatency: map[string]time.Duration{
+				"infra2": 30 * time.Millisecond,
+				"infra3": 30 * time.Millisecond,
+			},
+		},
+		{
+			Name:         "infra2",
+			ListenerAddr: "localhost:22381",
+			UpstreamAddr: "localhost:22380",
+			OutboundLatency: map[string]time.Duration{
+				"infra1": 30 * time.Millisecond,
+				"infra3": 10 * time.Millisecond,
+			},
+		},
+		{
+			Name:         "infra3",
+			ListenerAddr: "localhost:32381",
+			UpstreamAddr: "localhost:32380",
+			OutboundLatency: map[string]time.Duration{
+				"infra1": 30 * time.Millisecond,
+				"infra2": 10 * time.Millisecond,
+			},
+		},
 	},
 }
 
@@ -35,4 +69,43 @@ func TestParseExampleConfig(t *testing.T) {
 	config, err := ParseConfigBytes([]byte(exampleConfig))
 	require.NoError(t, err)
 	require.Equal(t, expectedExampleConfig, config)
+}
+
+func TestValidateConfig(t *testing.T) {
+	require.NoError(t, ValidateConfig(expectedExampleConfig))
+
+	require.ErrorContains(t, ValidateConfig(
+		&Config{
+			Listeners: []*Listener{
+				{Name: "dupe"},
+				{Name: "dupe"},
+			},
+		},
+	), "duplicate listener name dupe")
+
+	require.ErrorContains(t, ValidateConfig(
+		&Config{
+			Listeners: []*Listener{
+				{
+					Name: "bbq",
+					OutboundLatency: map[string]time.Duration{
+						"bbq": 100 * time.Millisecond,
+					},
+				},
+			},
+		},
+	), "listener bbq self reference in outbound latency")
+
+	require.ErrorContains(t, ValidateConfig(
+		&Config{
+			Listeners: []*Listener{
+				{
+					Name: "bbq",
+					OutboundLatency: map[string]time.Duration{
+						"nerp": 100 * time.Millisecond,
+					},
+				},
+			},
+		},
+	), "outbound latency for listener bbq references nerp which does not exist")
 }
